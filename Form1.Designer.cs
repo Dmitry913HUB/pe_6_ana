8b1fd05279fbfd2d4b96aeb7eb5f35e70176e88a
TenNM <tenmv@mail.ru>
2020-03-30 20:45:14 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace pe6
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.collectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.linearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.binaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelCollMode = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.collectionToolStripMenuItem,
+            this.processToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // collectionToolStripMenuItem
+            // 
+            this.collectionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.collectionToolStripMenuItem.Name = "collectionToolStripMenuItem";
+            this.collectionToolStripMenuItem.Size = new System.Drawing.Size(106, 29);
+            this.collectionToolStripMenuItem.Text = "Collection";
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.createToolStripMenuItem.Text = "Create";
+            this.createToolStripMenuItem.Click += new System.EventHandler(this.CreateToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.viewToolStripMenuItem.Text = "View";
+            this.viewToolStripMenuItem.Click += new System.EventHandler(this.ViewToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.EditToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.SaveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.loadToolStripMenuItem.Text = "Load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.LoadToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sortToolStripMenuItem,
+            this.seaToolStripMenuItem});
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(88, 29);
+            this.processToolStripMenuItem.Text = "Process";
+            // 
+            // sortToolStripMenuItem
+            // 
+            this.sortToolStripMenuItem.Name = "sortToolStripMenuItem";
+            this.sortToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.sortToolStripMenuItem.Text = "Sort";
+            // 
+            // seaToolStripMenuItem
+            // 
+            this.seaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.linearToolStripMenuItem,
+            this.binaryToolStripMenuItem});
+            this.seaToolStripMenuItem.Name = "seaToolStripMenuItem";
+            this.seaToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.seaToolStripMenuItem.Text = "Search";
+            // 
+            // linearToolStripMenuItem
+            // 
+            this.linearToolStripMenuItem.Name = "linearToolStripMenuItem";
+            this.linearToolStripMenuItem.Size = new System.Drawing.Size(162, 34);
+            this.linearToolStripMenuItem.Text = "Linear";
+            // 
+            // binaryToolStripMenuItem
+            // 
+            this.binaryToolStripMenuItem.Name = "binaryToolStripMenuItem";
+            this.binaryToolStripMenuItem.Size = new System.Drawing.Size(162, 34);
+            this.binaryToolStripMenuItem.Text = "Binary";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(78, 29);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 388);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // labelCollMode
+            // 
+            this.labelCollMode.AutoSize = true;
+            this.labelCollMode.Location = new System.Drawing.Point(341, 9);
+            this.labelCollMode.Name = "labelCollMode";
+            this.labelCollMode.Size = new System.Drawing.Size(108, 20);
+            this.labelCollMode.TabIndex = 2;
+            this.labelCollMode.Text = "labelCollMode";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelCollMode);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "Form Main";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem collectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sortToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem linearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem binaryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelCollMode;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pe6
+{
+    public partial class FormMain : Form
+    {
+        const string MODE_CREATE = "Mode: create";
+        const string MODE_VIEW = "Mode: view";
+        const string MODE_EDIT = "Mode: edit";
+        const string REALLY_CREATE_NEW = "Are you really want create new collection? Current collection will be removed.";
+        ArrayList list = new ArrayList();
+        public FormMain()
+        {
+            InitializeComponent();
+            //dataGridView1.RowHeadersVisible = false;//disable first column
+            dataGridView1.Columns.Add("abs", "abs");
+            dataGridView1.Columns.Add("fi", "fi");
+            dataGridView1.Columns[0].Width = 50;
+            dataGridView1.Columns[1].Width = 50;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            collView();
+
+            String[] arr1 = { "1", "1" };
+            dataGridView1.Rows.Add(arr1);
+            String[] arr2 = { "2", "2" };
+            dataGridView1.Rows.Add(arr2);
+
+        }
+        //--------------------------------------------------------------------------------------------service
+        private bool stringIsNumber(string s)
+        {
+            foreach (char c in s)
+            {
+                if (!char.IsDigit(c)) { return false; }
+            }
+            return true;
+        }
+        //--------------------------------------------------------------------------------------------
+        bool tableValidation()
+        {
+            //string s = "";
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                if(dataGridView1.Rows[i].Cells["abs"].Value != null &&
+                    dataGridView1.Rows[i].Cells["fi"].Value != null )
+                {
+                    if (!stringIsNumber(dataGridView1.Rows[i].Cells["abs"].Value.ToString()) &&
+                        !stringIsNumber(dataGridView1.Rows[i].Cells["fi"].Value.ToString()) )
+                    {
+                        //s += dataGridView1.Rows[i].Cells["abs"].Value.ToString() + " ";
+                        //s += dataGridView1.Rows[i].Cells["fi"].Value.ToString() + "\n";
+                        return false;
+                    }
+                }
+            }
+            //MessageBox.Show(s);
+            return true;
+        }
+        void tableToList()
+        {
+            CoTrigonometric ct = new CoTrigonometric();
+            if (tableValidation())
+            {
+                for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+                {
+                    ct.abs = Convert.ToDouble(dataGridView1.Rows[i].Cells["abs"].Value);
+                    ct.fi = Convert.ToDouble(dataGridView1.Rows[i].Cells["fi"].Value);
+                    list.Add(ct);
+                }
+            }
+        }
+        void showList()
+        {
+            string s = "";
+            foreach (CoTrigonometric ct in list)
+            {
+                s += ct.ToString() + "\n";
+            }
+            MessageBox.Show(s);
+        }
+        //----------------------------------------------------------------------------------------------
+        void collCreate()
+        {
+            DialogResult result = MessageBox.Show(REALLY_CREATE_NEW, "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if(result == DialogResult.Yes) {
+                dataGridView1.Rows.Clear();
+                labelCollMode.Text = MODE_CREATE;
+                dataGridView1.Columns["abs"].ReadOnly = false;
+                dataGridView1.Columns["fi"].ReadOnly = false;
+            }        
+        }
+        void collView()
+        {
+            labelCollMode.Text = MODE_VIEW;
+            dataGridView1.Columns["abs"].ReadOnly = true;
+            dataGridView1.Columns["fi"].ReadOnly = true;
+            
+        }
+        void collEdit()
+        {
+            labelCollMode.Text = MODE_EDIT;
+            dataGridView1.Columns["abs"].ReadOnly = false;
+            dataGridView1.Columns["fi"].ReadOnly = false;
+            
+        }
+        
+        //-----------------------------------------------------------------------------------------------
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Developed by TenNM", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            showList();
+        }
+
+        private void CreateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            collCreate();
+        }
+
+        private void ViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            collView();
+        }
+
+        private void EditToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            collEdit();
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tableToList();
+            //tableValidation();
+        }
+
+        private void LoadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
